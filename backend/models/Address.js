5a72c9aacfd870e2da43eb8903065522d68d067c
R0EYK <roeyk70@gmail.com>
2025-04-22 21:12:11 +0300

    Testing

--- backend/models/Address.js
+++ backend/models/Address.js
@@ -1,38 +1,36 @@
 import mongoose from "mongoose";
 
 const addressSchema = new mongoose.addressSchema({
-
-
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    fullName:{
-        type: String,
-        required: [true, "Full name is required"],
-        lowercase: true,
-    },
-    street:{
-        type: String,
-        required: [true, "Street is required"],
-    },
-    city:{
-        type: String,
-        required:[true,"city is required],
-    },
-    country: {
-        type: String,
-        required: [true, "Country is required"],
-    },
-    isDefault: {
-        type: Boolean,
-        default: false
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now
-    }
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  fullName: {
+    type: String,
+    required: [true, "Full name is required"],
+    lowercase: true,
+  },
+  street: {
+    type: String,
+    required: [true, "Street is required"],
+  },
+  city: {
+    type: String,
+    required: [true, "city is required"],
+  },
+  country: {
+    type: String,
+    required: [true, "Country is required"],
+  },
+  isDefault: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Address = mongoose.model("Address", addressSchema);
--- backend/models/Listing.js
+++ backend/models/Listing.js
@@ -1,42 +1,6 @@
 import mongoose from "mongoose";
 
 const listingSchema = new mongoose.listingSchema({
-     productId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Product",
-            required: true
-        },
-    sellerId:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true
-    },
-    saleType:{
-        type: String,
-        enum: ['auction', 'now'],
-        required: [true, "Sale type is required"]
-    },
-    price:{
-        type: Number
-    },
-    startingBid:{
-        type: Number
-    },
-    currentBid:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Bid",
-    },
-    expiredAt:{
-        type:Date
-    },
-    isSold:{
-        type: Boolean,
-        default: false
-    },
-    createdAt:{
-        type:Date,
-        default: Date.now
-    }
   productId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Product",
@@ -50,6 +14,7 @@ const listingSchema = new mongoose.listingSchema({
   saleType: {
     type: String,
     enum: ["auction", "now"],
+    required: [true, "Sale type is required"],
   },
   price: {
     type: Number,
