7afd51a20f6f59356f464ef56028c1ca33b23d35
R0EYK <roeyk70@gmail.com>
2025-04-22 20:30:49 +0300

    ESLint Install

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,28 +1,31 @@
-import mongoose from mongoose
+import mongoose from "mongoose";
 
 const orderSchema = new mongoose.orderSchema({
-    buyerId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required:[true,"buyerId is required"],
-        ref: "User",
-    },
-    sellerId:{
-        type: mongoose.Schema.Types.ObjectId,
-        required: [true,"sellerId is required"],
-        ref: "User",
-    },
-    listingId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required:[true,"Listing is required"],
-        ref: "Listing",
-    },
-    addressId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required:[true,"Address is required"],
-        ref: "Address",
-    },
-    price:{
-        type: Number,
-        required: [true, "Price is required"]
-    }
-})
+  buyerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, "buyerId is required"],
+    ref: "User",
+  },
+  sellerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, "sellerId is required"],
+    ref: "User",
+  },
+  listingId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, "Listing is required"],
+    ref: "Listing",
+  },
+  addressId: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, "Address is required"],
+    ref: "Address",
+  },
+  price: {
+    type: Number,
+    required: [true, "Price is required"],
+  },
+});
+
+const Order = mongoose.model("Order", orderSchema);
+export default Order;
--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,51 +1,49 @@
-import mongoose from mongoose
+import mongoose from "mongoose";
 
 const productSchema = new mongoose.productSchema({
-    ownerId: {
-        type: mongoose.Schema.Types.ObjectId,
-                    ref: "User",
-                    required: true
-    },
-    name: {
-        type: String,
-        required: true,
-    },
-    description:{
-        type: String,
-       
-    },
-    images: {
-        type: [String]
-    },
-    category: {
-        type: String,
-        enum: ['T-Shirt', 'Jeans','Sweatshirts'],
-        required:true,
-      },
-    brand: {
-        type: String,
-        required: true,
-    },
-    condition:{
-        type: String,
-        enum: ['New', 'Like New','Used'],
-        required:true
-    },
-    size:{
-        type: String,
-        required:true,
-    },
-    listing:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Listing",
-    },
-    createdAt:{
-        type: Date,
-        default: Date.now
-    },
-
+  ownerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  images: {
+    type: [String],
+  },
+  category: {
+    type: String,
+    enum: ["T-Shirt", "Jeans", "Sweatshirts"],
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  condition: {
+    type: String,
+    enum: ["New", "Like New", "Used"],
+    required: true,
+  },
+  size: {
+    type: String,
+    required: true,
+  },
+  listing: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Listing",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const Product = mongoose.model('Product', productSchema);
+const Product = mongoose.model("Product", productSchema);
 
 export default Product;
