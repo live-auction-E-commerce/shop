8e4968f711271cbb5087cb80162e8d0c1b98a74d
R0EYK <roeyk70@gmail.com>
2025-04-23 21:56:37 +0300

    small issues corrected

--- backend/models/PaymentIntent.js
+++ backend/models/PaymentIntent.js
@@ -1,35 +1,34 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const paymentIntentSchema = new mongoose.paymentIntentSchema({
+const paymentIntentSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
-    required: [true, "userId is required"],
-    ref: "User",
+    required: [true, 'userId is required'],
+    ref: 'User',
   },
   bidId: {
     type: mongoose.Schema.Types.ObjectId,
-    required: [true, "bidId is required"],
-    ref: "Bid",
+    required: [true, 'bidId is required'],
+    ref: 'Bid',
   },
   paymentMethodId: {
     type: mongoose.Schema.Types.ObjectId,
-    required: [true, "paymentMethodId is required"],
-    ref: "PaymentMethod",
+    required: [true, 'paymentMethodId is required'],
+    ref: 'PaymentMethod',
   },
   stripePaymentIntentId: {
     type: String,
-    required: [true, "stripePaymentIntentID is required"],
+    required: [true, 'stripePaymentIntentID is required'],
   },
   amount: {
-    type: number,
-    required: [true, "amount is required"],
+    type: Number,
+    required: [true, 'amount is required'],
   },
-  confirmedAt:{
+  confirmedAt: {
     type: Date,
-    default: Date.now
-}
-
+    default: Date.now,
+  },
 });
 
-const PaymentIntent = mongoose.model("PaymentIntent", paymentIntentSchema);
+const PaymentIntent = mongoose.model('PaymentIntent', paymentIntentSchema);
 export default PaymentIntent;
