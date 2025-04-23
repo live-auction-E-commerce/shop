81fd6e31969edcad84d41e182d10f16e9b2ed8ae
R0EYK <roeyk70@gmail.com>
2025-04-23 21:40:44 +0300

    Fix

--- backend/logic/ProductLogic.js
+++ backend/logic/ProductLogic.js
@@ -1,4 +1,4 @@
-import mongoose, { mongo } from 'mongoose';
+import mongoose from 'mongoose';
 import Product from '../models/Product';
 import { CategoryEnum, ConditionEnum, AllowedExtensions } from '../constants/enum';
 
