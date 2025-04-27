a566b69400f8113a43dfb9002fd75ecc2d59cb54
idanpopovich8 <idanpopovich8@gmail.com>
2025-04-27 15:46:45 +0300

    fixed order logic,controller and route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import ProductRoutes from './routes/ProductRoutes.js';
 import ListingRoutes from './routes/ListingRoutes.js';
 import AddressRoutes from './routes/AddressRoutes.js';
 import PaymentMethodRoutes from './routes/PaymentMethodRoutes.js';
+import OrderRoutes from './routes/OrderRoutes.js';
 import connectDB from './lib/db.js';
 
 dotenv.config();
@@ -18,6 +19,7 @@ app.use('/api', ProductRoutes);
 app.use('/api', ListingRoutes);
 app.use('/api', AddressRoutes);
 app.use('/api', PaymentMethodRoutes);
+app.use('/api', OrderRoutes);
 
 app.listen(5000, () => {
   console.log('Server is running on http://localhost:5000');
