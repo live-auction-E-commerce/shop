b28d80edd223b0eaf83b9cf2daf4dd1d2a6b3406
idanpopovich8 <idanpopovich8@gmail.com>
2025-04-28 15:28:12 +0300

    fixed logic

--- backend/routes/OrderRoutes.js
+++ backend/routes/OrderRoutes.js
@@ -7,7 +7,8 @@ router.post('/orders',OrderController.createOrder);
 router.get('/orders/:id',OrderController.getOrderById);
 router.get('/orders/seller/:id',OrderController.getAllOrdersBySeller);
 router.get('/orders/buyer/:id',OrderController.getAllOrdersByBuyer);
-router.get('/orders/user/:userId/:saleType',OrderController.getAllOrdersOfUserBySaleType);
-router.get('/orders/user/:userId',OrderController.getAllOrdersByUser);
+router.get('/orders/buyer/:buyerId/:saleType',OrderController.getAllOrdersOfBuyerBySaleType);
+router.get('/orders/seller/:sellerId/:saleType',OrderController.getAllOrdersOfSellerBySaleType);
+
 
 export default router;
