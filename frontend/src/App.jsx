da64808f20dc02d08da4158e425ca75c82fd937f
R0EYK <roeyk70@gmail.com>
2025-05-03 23:13:44 +0300

    small fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,5 @@
 import { ListingCard } from './components/ui/listing-card';
-import {
-  buyNowListing,
-  auctionListing,
-  soldListing,
-  currentBid,
-  sampleProduct,
-} from './constants/constants';
+import { buyNowListing, auctionListing, soldListing, sampleProduct } from './constants/constants';
 
 const handleBidClick = (listingId) => {
   console.log(`Placing bid on listing: ${listingId}`);
@@ -21,20 +15,13 @@ function App() {
       <h1 className="text-2xl font-bold mb-6">Marketplace Listings</h1>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        {/* Auction listing with current bid */}
         <ListingCard listing={auctionListing} product={sampleProduct} onBidClick={handleBidClick} />
-
-        {/* Buy now listing */}
         <ListingCard
           listing={buyNowListing}
           product={sampleProduct}
           onBuyNowClick={handleBuyNowClick}
         />
-
-        {/* Sold listing */}
         <ListingCard listing={soldListing} product={sampleProduct} />
-
-        {/* Loading state */}
         <ListingCard listing={soldListing} product={sampleProduct} isLoading={true} />
       </div>
     </main>
