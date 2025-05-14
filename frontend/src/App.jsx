5b71322022d2f6969bd9a31ece29e98b5140b114
idanpopovich8 <idanpopovich8@gmail.com>
2025-05-14 23:06:35 +0300

    fixed hero

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,6 @@ import AppLayout from '@/components/layout/AppLayout';
 import { Outlet } from 'react-router-dom';
 
 export default function App() {
-
-function App() {
-
   return (
     <AppLayout>
       <Outlet />
