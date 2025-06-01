88c4eca07b07d6999b36af65d6779793b05bae36
R0EYK <roeyk70@gmail.com>
2025-06-01 18:01:50 +0300

    Changed multiplying the amount by 100 to a const

--- backend/src/lib/payments.js
+++ backend/src/lib/payments.js
@@ -3,9 +3,12 @@ import config from '../../config.js';
 
 const stripe = new Stripe(config.STRIPE_SECRET_KEY);
 
+const CENTS_IN_DOLLAR = 100;
+// Stripe expects amount in the smallest currency unit, so for USD, cents.
+
 export const createStripePaymentIntent = async ({ amount, bidId, userId }) => {
   return await stripe.paymentIntents.create({
-    amount: amount * 100, // Stripe expects amount in cents
+    amount: amount * CENTS_IN_DOLLAR,
     currency: 'usd',
     capture_method: 'manual',
     payment_method_types: ['card'],
