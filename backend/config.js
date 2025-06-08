99eeb952f38cd9f5adbf97a147a0278000d85b0c
idanpopovich8 <idanpopovich8@gmail.com>
2025-06-08 19:43:40 +0300

    updated address crud

--- backend/config.js
+++ backend/config.js
@@ -8,7 +8,7 @@ const config = {
   BASE_URL:
     process.env.BASE_URL || `http://localhost:${process.env.PORT || 5001}`,
   MONGO_URI: process.env.MONGO_URI || '',
-  SECRET_KEY: process.env.SECRET_KEY || '',
+  JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || '',
   JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1d',
 };
 
@@ -16,7 +16,7 @@ if (!config.MONGO_URI) {
   console.warn('⚠️  Warning: MONGO_URI is not set in your .env file.');
 }
 
-if (!config.SECRET_KEY) {
+if (!config.JWT_SECRET_KEY) {
   console.warn(
     '⚠️  Warning: SECRET_KEY is not set in your .env file. JWT generation will fail.',
   );
