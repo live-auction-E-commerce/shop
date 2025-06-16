5745f7cb47274581c252bbfe6bc788078fa5b165
R0EYK <roeyk70@gmail.com>
2025-06-16 19:51:54 +0300

    magic date removed

--- backend/src/logic/ListingLogic.js
+++ backend/src/logic/ListingLogic.js
@@ -178,7 +178,7 @@ export const getAllListings = async (queryParams, req) => {
           {
             $or: [
               { saleType: { $ne: 'auction' } },
-              { expiredAt: { $gt: new Date() } },
+              { expiredAt: { $gt: now } },
             ],
           },
         ],
