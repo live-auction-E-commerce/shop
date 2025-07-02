6026a827af6ac63513f7a55e996997fd0e327dd4
idanpopovich8 <idanpopovich8@gmail.com>
2025-07-02 20:19:06 +0300

    fixed

--- frontend/src/components/layout/Header/Header.jsx
+++ frontend/src/components/layout/Header/Header.jsx
@@ -1,15 +1,22 @@
+/* eslint-disable no-unused-vars */
 import { Logo, CategoriesMenu, SearchBar, AuthSection } from './index';
+import { motion } from 'framer-motion';
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-gradient-to-r from-primary/10 to-secondary/10 shadow-sm flex h-16 items-center">
-      <div className="flex items-center justify-between w-full">
+    <motion.header
+      initial={{ x: '-100%', opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ type: 'spring', stiffness: 40, damping: 18, duration: 1.6 }}
+      className="sticky top-0 z-50 w-full border-b bg-gradient-to-r from-primary/10 to-secondary/10 shadow-sm flex h-16 items-center"
+    >
+      <div className="flex items-center justify-between w-full px-4">
         <Logo />
         <CategoriesMenu />
         <SearchBar />
         <AuthSection />
       </div>
-    </header>
+    </motion.header>
   );
 };
 
